62c3e69cf6fb6aa893a0c707a14a88649a04033c
BrunoOrtega9 <coub1908@gmail.com>
2023-04-13 18:36:52 -0600

    agregamos el codigo para visualizar imagen
    Signed-off-by: BrunoOrtega9 <coub1908@gmail.com>

--- Texto/testing_easyocr.py
+++ Texto/testing_easyocr.py
@@ -1,4 +1,10 @@
 import cv2
 import easyocr 
 
-reader = easyocr.Reader(["es"])
+reader = easyocr.Reader(["es"], gpu = False)
+
+image= cv2.imread("testing001.jpeg")
+
+cv2.imshow("Image", image)
+cv2.waitkey(0)
+cv2.destroyAllWindows()
